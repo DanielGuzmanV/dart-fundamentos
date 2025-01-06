33a1e4e47dd88161377b20cf7b665ddc621e4408
DLGuzBuho <109259673+DanielGuzmanV@users.noreply.github.com>
2025-01-06 18:43:07 -0400

    Se agrego y corrigio datos en listas y mapas

--- lib/fundamentos_intermedios/estructuras_de_datos/listas.dart
+++ lib/fundamentos_intermedios/estructuras_de_datos/listas.dart
@@ -1,8 +1,7 @@
 // Estructuras de datos:
 // --- Listas ---
 void main() {
-
-  // Podemos tener diferentes tipos de listas:
+  // Podemos crear diferentes tipos de listas:
   List<int> numerosEnteros = [10, 20, 30, 40, 50, 60];
   List<double> numerosDecimales = [1.1, 1.2, 1.3, 1.4, 1.5, 1.6];
   List<String> cadenas = ['uno', 'dos', 'tres', 'cuatro'];
@@ -12,37 +11,67 @@ void main() {
   print('El decimal es: ${numerosDecimales[5]}');
   print('La cadena es: ${cadenas[2]}');
 
+  print('---------------------------------------');
+
   // Agregar un nuevo elemento: esto agregara un numero al final de la lista
   numerosEnteros.add(50);
-  print(numerosEnteros);
+  print('Lista agrega es: $numerosEnteros');
 
   numerosDecimales.add(4.1);
-  print(numerosDecimales);
+  print('Lista agrega es: $numerosDecimales');
 
   cadenas.add('Seis');
-  print(cadenas);
+  print('Lista agrega es: $cadenas');
 
-  // Quitar un elemento: esto quitara cualquier elemento de la lista
+  print('---------------------------------------');
+
+  // Quitar un elemento
   numerosEnteros.remove(30);
-  print(numerosEnteros);
+  print('Lista sin un elemento es: $numerosEnteros');
 
   numerosDecimales.remove(1.2);
-  print(numerosDecimales);
+  print('Lista sin un elemento es: $numerosDecimales');
 
   cadenas.remove('tres');
-  print(cadenas);
+  print('Lista sin un elemento es: $cadenas');
+
+  print('---------------------------------------');
+
+  // Inserta un elemento por un indice
+  numerosEnteros.insert(3, 99); // Aqui decimos que en el indice 3 se agregara el 99
+  print('Los nuevos elementos son: $numerosEnteros');
+
+  print('---------------------------------------');
+
+  // Verificamos la longitud de las listas
+  // Tenemos dos formas de hacerlo, la primera:
+  int lengthList = numerosEnteros.length;
+  print('La longitud de la lista de numeros enteros es: $lengthList');
+
+  // La segunda:
+  print('La longitud de la lista de numeros decimales es: ${numerosDecimales.length}');
+
+  print('La longitud de la lista de cadenas es: ${cadenas.length}');
+
+  print('---------------------------------------');
 
   // Recorrer una lista:
-  // Primera forma:
-  for(int idx in numerosEnteros) {
+  // Primera forma: numeros enteros
+  for (int idx in numerosEnteros) {
     print('Los enteros son: $idx');
   }
 
-  // Segunda forma: con ( .length ) verificamos la longitud de la lista
-  for(int idx = 0; idx <= numerosDecimales.length - 1; idx++){
+  print('---------------------------------------');
+
+  // Segunda forma: numeros decimales con ( .length )
+  for (int idx = 0; idx <= numerosDecimales.length - 1; idx++) {
     double content = numerosDecimales[idx];
     print('Los decimales son: $content');
   }
 
-}
+  print('---------------------------------------');
 
+  for (String idx in cadenas) {
+    print('La cadena es: $idx');
+  }
+}
--- lib/fundamentos_intermedios/estructuras_de_datos/mapas.dart
+++ lib/fundamentos_intermedios/estructuras_de_datos/mapas.dart
@@ -2,7 +2,6 @@
 // --- Mapas o Diccionarios ---
 
 void main() {
-
   // Creamos nuevos mapas:
   Map<String, int> edades = {
     'Martin': 23,
@@ -21,16 +20,45 @@ void main() {
   edades['Fabian'] = 32;
   print(edades);
 
-  // Eliminar una clave: 
+  // Eliminar una clave:
   edades.remove('Marcelo');
   print(edades);
 
   // Recorrer un mapa
   edades.forEach((clave, valor) {
-    print('$clave tiene $valor anos');
+    print('$clave tiene $valor años');
   });
 
-  // Tambien con ( map.length ) podemos saber la longitud del mapa
+  // Verificamos si contiene una clave
+  bool verificarClave = edades.containsKey('Marta'); // Podemos cambiar el nombre por otro para verificar
+  print('Existe la edad: $verificarClave');
 
+  // Verificamos la longitud del mapa
+  int lengthMap = edades.length;
+  print('El mapa tiene $lengthMap valores');
+
+  print('------------------------------------------------');
+
+  // Manejo de un mapa dentro de un mapa: <Map<String, int>> o <String<String, int>>
+  Map<String, Map<String, int>> mercado = {
+    'Frutas': {'Manzanas': 5, 'Naranjas': 3, 'Duraznos': 2},
+    'Verduras': {'Zanahorias': 7, 'Espinacas': 2, 'Pimenton': 5},
+  };
+
+  // Accedemos a los valores de: Frutas - Manzanas y Verduras - Zanahorias
+  print('La cantidad es: ${mercado['Frutas']?['Manzanas']}'); // El nombre dentro de las llaves tiene que ser igual al que esta en el mapa
+  print('La cantidad es: ${mercado['Verduras']?['Zanahorias']}');
+
+  print('------------------------------------------------');
+
+  // Manejo de lista dentro de un mapa
+  Map<String, List<String>> grupos = {
+    'Amigos': ['Marco', 'Pedro', 'Anna'],
+    'Familia': ['Fernando', 'Manuel']
+  };
+
+  // Accedemos al grupo, ya sea de familia o amigos
+  print(grupos['Amigos']);
+  print(grupos['Familia']);
 
 }
