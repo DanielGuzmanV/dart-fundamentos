d9f48681db75ccb5a5591abfd5b64ae55eede285
DLGuzBuho <109259673+DanielGuzmanV@users.noreply.github.com>
2025-01-21 11:06:34 -0400

    se termino el ejemplo 4

--- lib/ejemplos/listas/ejemplo4.dart
+++ lib/ejemplos/listas/ejemplo4.dart
@@ -15,8 +15,8 @@ void main() {
     print('2. Eliminar numero');
     print('3. Mostras numeros');
     print('4. Operacion');
-    print('5. Reducir los valores');
-    print('6. Suma de todos los valroes');
+    print('5. Verificar par e impar y primos');
+    print('6. Suma de todos los valores');
     print('7. Salir');
 
     String? opcion = stdin.readLineSync();
@@ -74,7 +74,7 @@ void main() {
       print('----------------------------');
 
       if(listaNumeros.isEmpty) {
-        print('La lista esta vacia');
+        print(' -- La lista esta vacia --');
       } else {
 
         // Podemos mostrar los elementos de la lista:
@@ -86,6 +86,8 @@ void main() {
         print('los numeros de la lista son: $listaNumeros');
 
       }
+
+    // ----------------------------------------------------
     }else if(opcion == '4') {
       print('Escoga la operacion: ');
       print('1. Suma');
@@ -139,9 +141,44 @@ void main() {
         print('Opcion invalida...');
       }
 
+    // ----------------------------------------------------
+    } else if(opcion == '5') {
+      print('Seleccione una opcion:');
+      print('1. Verificar pares de la lista');
+      print('2. verificar impares de la lista');
+      print('3. verificar si es primo');
 
+      String? selecNumber = stdin.readLineSync();
+      if(listaNumeros.isNotEmpty){
+        if (selecNumber == '1') {
+          List<int> pares = listaNumeros.where(esPar).toList();
+          print('Los pares son: $pares');
+        } else if (selecNumber == '2') {
+          List<int> impares = listaNumeros.where(esImpar).toList();
+          print('Los impares son: $impares');
+        } else if (selecNumber == '3') {
+          List<int> primos = listaNumeros.where(selecPrimo).toList();
+          print('Los primos son: $primos');
+        } else {
+          print('Opcion no valida');
+        }
+      } else{
+        print('- Lista vacia, primero agregue numeros a la lista');
+      }
+    // ----------------------------------------------------
+    } else if(opcion == '6') {
+      if(listaNumeros.isNotEmpty){
+
+        int sumaNumeros = listaNumeros.reduce((valor, elemento) {
+          int resultado = valor + elemento;
+          return resultado;
+        });
+
+        print('La suma de la lista: $listaNumeros es igual a $sumaNumeros');
+      } else {
+        print('Lista sin valores, no se puede realizar operacion');
+      }
     }
-    
     else if(opcion == '7'){
       print('Saliendo...');
       active = false;
@@ -154,10 +191,40 @@ void main() {
 
 }
 
+// Calcular Suma
 int calcularSuma(int number1, int number2 ) {
   return number1 + number2;
 }
 
+// Calcular Multiplicacion
 int calcularMultiplicacion(int number1, int number2 ) {
   return number1 * number2;
 }
+
+// Seleccionar numeros pares de la lista
+bool esPar(int number1) {
+  return number1 % 2 == 0;
+}
+
+// Seleccionar numeros impares de la lista
+bool esImpar(int number1) {
+  return number1 % 2 != 0;
+}
+
+// Seleccionar primos
+bool selecPrimo(int number1) {
+  bool esPrimo = true;
+  if(number1 <= 1) {
+    esPrimo = false; 
+  } else {
+    for(int idx = 2; idx <= number1 ~/ 2; idx++) {
+      if(number1 % idx == 0) {
+        esPrimo = false;
+        break;
+      }
+    }
+  }
+
+  return esPrimo;
+}
+
