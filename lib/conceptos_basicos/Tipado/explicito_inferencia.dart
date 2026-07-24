2a21973bac4e76f52b07637eac0855638749c5ed
DLGuzBuho <109259673+DanielGuzmanV@users.noreply.github.com>
2026-07-23 21:36:20 -0400

    leves cambios en el file tipado

--- lib/conceptos_basicos/Tipado/explicito_inferencia.dart
+++ lib/conceptos_basicos/Tipado/explicito_inferencia.dart
@@ -2,22 +2,24 @@ void main() {
 
 // Tipado Explicito
 // Le dices al compilador exactamente qué tipo de dato vas a guardar.
+  print('\n=== Tipado Explicito ===');
   String nombre = 'Alberto';
   int edad = 23;
   double altura = 1.65;
   bool esDesarrollador = true;
 
-  print(nombre);
-  print(edad);
-  print(altura);
-  print(esDesarrollador);
+  print('Nombre: $nombre');
+  print('Edad: $edad');
+  print('Altura: $altura');
+  print('Es desarrollador?: $esDesarrollador');
 
   // Inferencia de tipo
   // Con var, el compilador deduce el tipo según el valor que le asignas 
   // en la primera línea. Pero ojo: sigue siendo tipado estático.
+  print('\n=== Inferencia de tipo ===');
   var lenguaje = "Dart";
   var version = 5.10;
 
-  print(lenguaje);
-  print(version);
+  print('Tipo de lenguaje: $lenguaje');
+  print('Version: $version');
 }
